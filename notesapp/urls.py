c28c041e5d0e1253b7ad6ca50730429e170e7f67
martinsmandina <martinsmandina@gmail.com>
2020-12-10 07:55:24 +0300

    add views and template

---
+++ notesapp/urls.py
@@ -0,0 +1,16 @@
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path, re_path
+
+
+urlpatterns = [
+    path('',views.main, name='home'),
+    path('note_delete/<int:noteid>/', views.delete_note, name='note_delete'),
+
+]
+
+
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- notesapp/views.py
+++ notesapp/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Notes
 
 # Create your views here.
+
+def main(request):
+    noteid = int(request.GET.get('noteid', 0))
+    notes = Notes.objects.all()
+
+    if request.method == 'POST':
+        noteid = int(request.POST.get('noteid', 0))
+        title = request.POST.get('title')
+        content = request.POST.get('content', '')
+
+        if noteid > 0:
+            note = Document.objects.get(pk=noteid)
+            note.title = title
+            note.content = content
+            note.save()
+            return redirect('/?noteid=%i' % noteid)
+        else:
+            note = Notes.objects.create(title=title, content=content)
+            return redirect('/?noteid=%i' % note.id)
+
+    if noteid > 0:
+        note = Notes.objects.get(pk=noteid)
+    else:
+        note = ''
+        return render(request,'main.html',{'notes': notes, 'note': note,'noteid':noteid})
+
+def delete_note(request, noteid):
+    note = Notes.objects.get(pk=noteid)
+    note.delete()
+
+    return redirect('/?noteid=0')
