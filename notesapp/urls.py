26f856d4895ed33f8ef2baa99fae8a6562770922
martinsmandina <martinsmandina@gmail.com>
2020-12-10 13:15:10 +0300

    add end points for single note

--- notesapp/urls.py
+++ notesapp/urls.py
@@ -2,12 +2,16 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, re_path
+from django.conf.urls import url,include
+
 
 
 urlpatterns = [
     path('',views.main, name='home'),
     path('note_delete/<int:noteid>/', views.delete_note, name='note_delete'),
-    path('api/noteall/', views.NoteList.as_view(), name='notes') 
+    path('api/noteall/', views.NoteList.as_view(), name='notes'),
+    url(r'api/noteall/noteid/(?P<pk>[0-9]+)/$',views.Notesingle.as_view(),name='singlenote')
+
 ]
 
 if settings.DEBUG:
--- notesapp/views.py
+++ notesapp/views.py
@@ -4,6 +4,8 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Notes
 from .serializer import NoteSerializer
+from rest_framework import status
+
 
 
 
@@ -61,3 +63,32 @@ class NoteList(APIView):
             serializers.save()
             return Response(serializers.data, status=status.HTTP_201_CREATED)
         return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class Notesingle(APIView):
+    # permission_classes = (IsAdminOrReadOnly,)
+    def get_note(self, pk):
+        try:
+            return Notes.objects.get(pk=pk)
+        except Notes.DoesNotExist:
+            return Http404
+
+    def get(self, request, pk, format=None):
+        note = self.get_note(pk)
+        serializers = NoteSerializer(note)
+        return Response(serializers.data)
+
+    def put(self, request, pk, format=None):
+        note = self.get_note(pk)
+        serializers = NoteSerializer(note, request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data)
+        else:
+            return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        note = self.get_note(pk)
+        note.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
